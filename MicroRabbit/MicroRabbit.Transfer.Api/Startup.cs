bf7cfa5a6b5feab830a8045cdc2259bac8b9f036
Johan <johan.ccs@gmail.com>
2021-09-03 15:54:51 +0200

    Test Jenkins

--- MicroRabbit/MicroRabbit.Transfer.Api/Startup.cs
+++ MicroRabbit/MicroRabbit.Transfer.Api/Startup.cs
@@ -40,7 +40,7 @@ namespace MicroRabbit.Transfer.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            //Test Jenkins
             app.UseHttpsRedirection();
             app.UseMvc();
         }
